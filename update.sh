3b7cbb2e6bc567638e277421dd9a2b6294a7fedd
Wang Jinchao <wangjinchao600@gmail.com>
2025-04-28 19:26:16 +0800

    save debug config

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+time make O=out -j$(nproc) deb-pkg
+ssh qemu "rm -rf /root/debs/*.deb"
+scp *.deb qemu:/root/debs/
+ssh qemu "cd /root/debs; dpkg -i *.deb"
+rm *.deb
+rm *.dsc
+rm *.buildinfo
+rm *.changes
+rm linux-upstream*.tar.gz
